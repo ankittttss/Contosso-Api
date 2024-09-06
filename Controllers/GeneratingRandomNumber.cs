d1365ea30c38530714b42358f9b17a71f51b1ed9
ankittttss <ankitsaini955831@gmail.com>
2024-09-06 16:22:06 +0530

    API

---
+++ Controllers/GeneratingRandomNumber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GeneratingRandomNumberController : ControllerBase
+    {
+        private readonly Random _random = new Random();
+
+        // GET: api/GeneratingRandomNumber
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Generate a random number between 1 and 100 (inclusive)
+            int randomNumber = _random.Next(1, 101);
+
+            // Return the number as JSON
+            return Ok(new { Number = randomNumber });
+        }
+    }
+}
